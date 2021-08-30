0f2cc67d66495240137e95e14d84e25b87c9d17a
Natalia Romashko <sharkellgm@gmail>
2021-08-30 22:31:25 +0300

    init: add init dependencies and files

---
+++ client/components/makeChoice.tsx
@@ -0,0 +1,47 @@
+import { Button,  makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    width: '500px',
+    flexDirection: 'column',
+    backgroundColor: '#a7f8a0',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: 'auto',
+    height: 'calc(100vh / 2)',
+    padding: '10px',
+  },
+  buttons_wrapper: {
+    marginTop: '50px',
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  redBtn: {
+    backgroundColor: 'red',
+    color: '#fff',
+  }
+});
+
+interface MakeChoiceProps {
+  choice: (color: string) => void;
+  message: string
+}
+
+export const MakeChoice = ({choice, message}: MakeChoiceProps) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h3" color="primary" align='center'>
+        {message}
+      </Typography>
+      <div className={classes.buttons_wrapper}>
+        <Button variant='contained' color='primary' onClick={() => choice('blue')}>Blue Peel</Button>
+        <Button variant='contained' className={classes.redBtn}  onClick={() => choice('red')}>Red Peel</Button>
+      </div>
+    </div>
+  );
+};
