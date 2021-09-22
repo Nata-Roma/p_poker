7bd2ce9e01cdbe5a1645a2a1ccbb5dbed1ee4b02
Natalia Romashko <sharkellgm@gmail>
2021-09-22 16:12:15 +0300

    ssr fetch-12

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -25,48 +25,28 @@ const HomePage = (props: HomePageProps) => {
 };
 
 export const getServerSideProps = async () => {
-
-  const prod = await fetch('https://fakestoreapi.com/products');
-  let products = await prod.json();
-
   try {
+    const res = await apiGetRooms();
+    const data = await res.data;
 
-    
-
-    const fRes = await fetch('http://localhost:4000/rooms');
-    console.log('fRes', fRes);
-
-    const fResData = await fRes.json()
-    console.log('fResData', fResData);
-
-    // const res = await apiGetRooms();
-    // const data = await res.data;
-    
-    
-    if (fRes.status === 200) {
-      if (typeof fResData === 'string') {
+    if (res.status === 200) {
+      if (typeof data === 'string') {
         return {
           props: {
-            rooms: [],
-            response: fResData,
-            prod: products[0]
+            rooms: []
           }
         }
       }
       return {
         props: {
-          rooms: fResData,
-          response: fResData,
-          prod: products[0]
+          rooms: res.data
         }
       }
     }
   } catch {
     return {
       props: {
-        rooms: [],
-        response: 'fake',
-        prod: products[0]
+        rooms: []
       }
     }
   }
