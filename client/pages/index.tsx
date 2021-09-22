433327ba2fd227bac118af6bc448054aaead2ad2
Natalia Romashko <sharkellgm@gmail>
2021-09-22 14:29:36 +0300

    ssr fetch-3

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,12 +5,16 @@ import { IRoomInfo } from 'utils/interfaces';
 interface HomePageProps {
   rooms: Array<IRoomInfo>,
   response: Array<IRoomInfo> | string
+  prod: any
 }
 
 const HomePage = (props: HomePageProps) => {
-  const {rooms, response} = props;
+  const {rooms, response, prod} = props;
   console.log('rooms', rooms);
   console.log('response', response);
+  console.log('prod');
+  console.log(prod);
+  
   
   
   return (
@@ -21,7 +25,25 @@ const HomePage = (props: HomePageProps) => {
 };
 
 export const getServerSideProps = async () => {
+
+  const fake = {
+    "id": 1,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  }
+
+
   try {
+
+    const prod = await fetch('https://fakestoreapi.com/products');
+    let products = await prod.json();
 
     const fRes = await fetch('http://localhost:4000/rooms');
     console.log('fRes', fRes);
@@ -32,19 +54,22 @@ export const getServerSideProps = async () => {
     const res = await apiGetRooms();
     const data = await res.data;
 
+    
     if (res.status === 200) {
       if (typeof data === 'string') {
         return {
           props: {
             rooms: [],
-            response: fResData
+            response: fResData,
+            prod: products[0]
           }
         }
       }
       return {
         props: {
           rooms: fResData,
-          response: fResData
+          response: fResData,
+          prod: products[0]
         }
       }
     }
@@ -52,7 +77,8 @@ export const getServerSideProps = async () => {
     return {
       props: {
         rooms: [],
-        response: 'no rooms'
+        response: 'no rooms',
+        prod: fake
       }
     }
   }
